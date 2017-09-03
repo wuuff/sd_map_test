64fc4af0d0164ad84a1a8933ff28d47ebfca975a
wuuff <wuuuufff@gmail.com>
2017-09-03 15:58:18 -0500

    Initial commit with simple hello world program

---
+++ sd_map_test.ino
@@ -0,0 +1,16 @@
+#include <Gamebuino.h>
+
+Gamebuino gb;
+
+void setup() {
+  gb.begin();
+  gb.titleScreen(F("SD MAP TEST"));
+}
+
+void loop() {
+  if( gb.update() ){
+    gb.display.cursorX = 0;
+    gb.display.cursorY = 0;
+    gb.display.print(F("Hello World"));
+  }
+}
